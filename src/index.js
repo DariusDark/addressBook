94f309f90b000216553bf43df32fa8e95872c232
DariusDark <darius99dark@mail.ru>
2022-03-10 09:47:00 +0500

    FEATURE: added google analytics 4

--- src/index.js
+++ src/index.js
@@ -3,8 +3,14 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import ReactGA from 'react-ga4';
 import { Provider } from 'react-redux';
 import store from './store/Store.jsx';
+
+const TRACKING_ID = 'G-DZ2W3DE56N';
+ReactGA.initialize(TRACKING_ID);
+ReactGA.send('pageview');
+
 ReactDOM.render(
   <Provider store={store}>
     <Router>
